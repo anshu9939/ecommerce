ccfcc1117056a3a3e8aa360c3353d9f89c32d84f
anshu singh <anshusingh8502@gmail.com>
2023-10-26 21:08:31 +0530

    first commit

---
+++ src/customer/Components/Carousel/HomeCaroselData.js
@@ -0,0 +1,20 @@
+export const homeCarouselData=[
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/_/1_7.jpg",
+        path:"/women/clothing/lengha_choli"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/4/_/4_11.jpg",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/9/_/9_8.jpg",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/2/_/2_11.jpg",
+        path:"/women/clothing/women_saree"
+    }
+
+]
+
